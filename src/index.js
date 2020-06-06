92c2f533e642218cfdcf73ce3d0ca4d718f529f8
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-06-06 01:05:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchDogs()
+  fetchBreeds()
+})
+
+function fetchDogs() {
+fetch('https://dog.ceo/api/breeds/image/random/4')
+	.then(resp => resp.json())
+	.then(json => renderDogs(json))
+}
+
+function fetchBreeds() {
+	fetch('https://dog.ceo/api/breeds/list/all')
+	.then(resp => resp.json())
+	.then(json => renderBreeds(json))
+}
+
+
+function renderDogs(dogs) {
+	const div = document.querySelector('div')
+	dogs.message.forEach(dog => {
+		const img = document.createElement('img')
+		img.src = dog
+		div.appendChild(img)
+	})
+}
+
+function renderBreeds(breeds) {
+	const ul = document.querySelector('ul')
+	const array = []
+		Object.keys(breeds.message).forEach(breed => {
+			const li = document.createElement('li')
+			li.innerHTML = breed
+			ul.appendChild(li)
+			li.addEventListener("click", function(){
+				this.style.color = 'red';
+			})
+			array.push(breed)
+		})
+
+	let filter = document.getElementById('breed-dropdown')	
+	filter.addEventListener('change', function() {
+		if (filter.value === "b") {
+			console.log("hi")
+		}
+		else if (filter.value === "a") {
+			console.log("hey")
+		}
+		else if (filter.value === "c") {
+			console.log("sup")
+		}
+		else if (filter.value === "d") {
+			console.log("yo")
+		}
+		else console.log("all")
+	})
+}
+
+
+
+
+
+
