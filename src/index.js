716c761c3d1be1deb1135e70365bb70887684577
Wills Blake <richwblake@gmail.com>
2020-06-11 23:11:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+function fetchDogImages() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(json => renderDogImages(json.message))
+}
+
+function renderDogImages (images) {
+    const imageContainer = document.querySelector('#dog-image-container');
+    images.forEach(imageURL => {
+        const img = document.createElement('img');
+        img.src = imageURL;
+        imageContainer.appendChild(img);
+    });
+}
+
+
+
+window.addEventListener("DOMContentLoaded", event => {
+    console.log('%c DOM has been successfully loaded!', 'color: green');
+    fetchDogImages();
+    console.log('%c Images have finished loading', 'color: green');
+});
