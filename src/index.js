efbd0c0dbaa6cc1523827dcb30b8dcaa5325a4ba
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-15 00:58:32 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
 function apiRequest(url){
-  
+  return fetch(url)
+    .then(resp => resp.json())
+    .then(json => addImg(json))
 }
 
 
 
+function addImg(obj){
+  obj.forEach(obj => {
+    
+  })
+}
 
-
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){  //Listening for on page load
+  apiRequest(imgUrl)
   
   
   
