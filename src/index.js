52fbc5dc4fa54cad3b8f6b6434465d6c0fb687a0
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-15 00:32:14 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function apiRequest(url){
+  
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  
+  
+  
+  
+})
